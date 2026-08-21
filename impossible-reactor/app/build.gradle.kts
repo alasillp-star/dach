790f7327e89836c370aadf940da2c206ec8ed710
alasillp-star <alasillp@gmail.com>
2026-08-22 00:48:20 +0200

    Add reactor app build file

---
+++ impossible-reactor/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.oryx.impossiblereactor"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.oryx.impossiblereactor"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
